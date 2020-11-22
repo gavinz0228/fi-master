5b5af01d22bc40cb89f73c90ef56253baa0b7ce6
gavin <gavinz0228@gmail.com>
2020-11-22 16:06:37 -0500

    initial commit, included US tresury yield fetching functions

---
+++ src/fimaster/utils/__init__.py
@@ -0,0 +1,2 @@
+from .request import *
+from .date_utils import *
---
+++ src/tests/test_api.py
@@ -0,0 +1,40 @@
+import sys
+from os import path
+from datetime import date, datetime
+sys.path.append(path.join(path.dirname(__file__), '..'))
+
+from fimaster.utils import get, is_bday
+from fimaster.data_api import *
+
+def test_get():
+    print("get xml from us treasury gov")
+    url = "https://www.treasury.gov/resource-center/data-chart-center/interest-rates/pages/XmlView.aspx?data=yield"
+    assert len(get(url)) > 0
+
+def test_bus_day_check():
+    assert is_bday(datetime(2020, 11, 1)) == False
+    assert is_bday(datetime(2020, 11, 2)) == True
+
+def test_get_one_day():
+    print('get a day')
+    data = get_yield_curve(2020, 11, 2)
+    assert len(data) > 0
+
+def test_get_month():
+    print('get a month')
+    data = get_yield_curve(2020, 11)
+    assert len(data) > 0
+
+def test_get_year():
+    print('get curr year')
+    data = get_yield_curve(2020)
+    assert len(data) > 0
+
+def test_get_multiple_dates():
+    print('get multiple dates')
+    data = get_yield_curve_by_dates([datetime(2020, 7, 2), datetime(2020, 11, 2), datetime(2020, 10, 2),datetime(2020, 9, 2)])
+    assert len(data) > 0
+
+
+if __name__ == '__main__':
+    test_get()
